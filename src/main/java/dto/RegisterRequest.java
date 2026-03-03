86fb2b1d05375e690cb17d49c3c8a0d9a1873dd6
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-03 13:48:15 +0530

    RegisterRequest file build.

---
+++ src/main/java/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package dto;
+
+import entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data // Lombok: getter and setter
+public class RegisterRequest {
+    @NotBlank(message = "Username is required") // validation cannot be empty
+    @Size(min = 3, max = 50, message = "Username must be 3-50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at leats 6 characters")
+    private String password;
+
+    private User.Role role; // // Optional — defaults to VIEWER if not provided
+}
