aeb355b524a7e4e8f1e3d744aff137c0014e2ff3
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-03 16:21:28 +0530

    This filter intercepts every HTTP request and checks for a valid token.

---
+++ src/main/java/security/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+        throws ServletException, IOException {
+
+        // 1. Get the Authorization header
+        String authHeader = request.getHeader("Authorization");
+
+        // 2.If no token, skip (Spring Security will handle it as anonymous)
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 3. Extract the token (remove "Bearer " prefix)
+        String token = authHeader.substring(7);
+
+        // 4. Validate and set authentication in Spring's security context
+        if (jwtUtil.isTokenValid(token)) {
+            String username = jwtUtil.extractUsername(token);
+            String role = jwtUtil.extractRole(token);
+
+            // tell spring security "This user is authenticated with this role"
+            UsernamePasswordAuthenticationToken authentication
+                    = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    List.of(new SimpleGrantedAuthority("ROLE_" + role))
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        // 5. Continue to the next fillter
+        filterChain.doFilter(request, response);
+    }
+}
