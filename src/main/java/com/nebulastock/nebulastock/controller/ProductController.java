9229669d4e82b959e0cdd499d4dbb9f06f625495
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-04 17:52:34 +0530

    ProductController file done.

---
+++ src/main/java/com/nebulastock/nebulastock/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.nebulastock.nebulastock.controller;
+
+import com.nebulastock.nebulastock.dto.request.ProductRequest;
+import com.nebulastock.nebulastock.dto.response.ProductResponse;
+import com.nebulastock.nebulastock.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    //Only Admin can create post
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(request));
+    }
+
+    // Anyone authenticated can view products
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getAll(
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) String category,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(productService.getAll(search, category, page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(productService.getById(id));
+    }
+
+    // Only ADMIN or MANAGER can update
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+    public ResponseEntity<ProductResponse> update(
+            @PathVariable Integer id,
+            @Valid @RequestBody ProductRequest request) {
+        return ResponseEntity.ok(productService.update(id, request));
+    }
+
+    //Only ADMIN can delete
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build(); // 204 No Content — standard for DELETE
+    }
+}
